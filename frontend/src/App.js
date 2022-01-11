442887444700a06ff6961c30dae6e7aec6c1cea9
sonali <sonalimore773@gmail.com>
2022-01-12 00:49:41 +0530

    Logout and Regsiter Page Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-import LoginScreen from './screens/LoginScreen'
-
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 function App() {
   return (
     <Router>
@@ -15,6 +15,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,8 +2,12 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-} from '../constants/userConstants'
-import axios from 'axios'
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_LOGOUT,
+} from "../constants/userConstants"
+import axios from "axios"
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
@@ -11,11 +15,11 @@ export const login = (email, password) => async (dispatch) => {
     })
     const config = {
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
     }
     const { data } = await axios.post(
-      '/api/users/login',
+      "/api/users/login",
       { email, password },
       config
     )
@@ -24,10 +28,52 @@ export const login = (email, password) => async (dispatch) => {
       payload: data,
     })
 
-    localStorage.setItem('userInfo', JSON.stringify(data))
+    localStorage.setItem("userInfo", JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
+      payload:
+        error.repsonse && error.repsonse.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo")
+  dispatch({ type: USER_LOGOUT })
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    })
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+    const { data } = await axios.post(
+      "/api/users",
+      { name, email, password },
+      config
+    )
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    })
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+
+    localStorage.setItem("userInfo", JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
       payload:
         error.repsonse && error.repsonse.data.message
           ? error.response.data.message
